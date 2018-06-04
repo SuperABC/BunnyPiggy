557e9ef9496299c848ad9b2ba9989a3fa24af1de
SG master <1363028533@qq.com>
2018-06-04 23:18:28 +0800

    Implement a little in lover.

--- BunnyPiggy/app/src/main/java/supergp/bunnypiggy/DailyActivity.java
+++ BunnyPiggy/app/src/main/java/supergp/bunnypiggy/DailyActivity.java
@@ -45,16 +45,16 @@ public class DailyActivity extends AppCompatActivity{
         setContentView(R.layout.activity_daily);
 
         gridView = (GridView) findViewById(R.id.gridview);
-        dataList = new ArrayList<Map<String, Object>>();
+        dataList = new ArrayList<>();
         String name[]={
                 "早安猪猪","一起吃早饭呀","每日一爱",
                 "小树参天","美食博主","午觉打卡",
                 "发呆想猪","运动一刻","日常怼猪",
                 "痴兔幻想","小兔崽子","早点睡啦"};
-        for (int i = 0; i <name.length; i++) {
-            Map<String, Object> map=new HashMap<String, Object>();
+        for (String aName : name) {
+            Map<String, Object> map = new HashMap<>();
             map.put("img", R.mipmap.ic_launcher);
-            map.put("text",name[i]);
+            map.put("text", aName);
             dataList.add(map);
         }
         String[] from={"img", "text"};
@@ -99,11 +99,11 @@ public class DailyActivity extends AppCompatActivity{
                         showPopupPic();
                         break;
                     case 2:
-                        final EditText input = new EditText(DailyActivity.this);
-                        AlertDialog.Builder builder = new AlertDialog.Builder(DailyActivity.this);
-                        builder.setTitle("每天爱一次").setIcon(android.R.drawable.ic_input_get)
-                                .setView(input).setNegativeButton("取消", null);
-                        builder.setPositiveButton("我爱猪猪", new DialogInterface.OnClickListener() {
+                        final EditText loveInput = new EditText(DailyActivity.this);
+                        AlertDialog.Builder loveBuilder = new AlertDialog.Builder(DailyActivity.this);
+                        loveBuilder.setTitle("每天爱一次").setIcon(android.R.drawable.ic_input_get)
+                                .setView(loveInput).setNegativeButton("取消", null);
+                        loveBuilder.setPositiveButton("我爱猪猪", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 new Thread(new Runnable() {
                                     @Override
@@ -112,7 +112,7 @@ public class DailyActivity extends AppCompatActivity{
                                         try {
                                             socket = new Socket("192.168.1.184", 4497);
 
-                                            String socketData = "daily2:" + input.getText().toString();
+                                            String socketData = "daily2:" + loveInput.getText().toString();
                                             BufferedWriter writer = new BufferedWriter(
                                                     new OutputStreamWriter(socket.getOutputStream()));
                                             writer.write(socketData + "\0");
@@ -134,9 +134,11 @@ public class DailyActivity extends AppCompatActivity{
                                 }).start();
                             }
                         });
-                        builder.show();
+                        loveBuilder.show();
                         break;
                     case 3:
+                        Toast.makeText(getApplicationContext(), "要等猪猪把树种下去才可以玩哟~",
+                                Toast.LENGTH_SHORT).show();
                         break;
                     case 4:
                         showPopupPic();
@@ -171,18 +173,121 @@ public class DailyActivity extends AppCompatActivity{
                         }).start();
                         break;
                     case 6:
+                        Toast.makeText(getApplicationContext(), "嘿嘿，我也在想你~",
+                                Toast.LENGTH_SHORT).show();
                         break;
                     case 7:
+                        Intent intent = new Intent(DailyActivity.this, SportActivity.class);
+                        startActivity(intent);
                         break;
                     case 8:
+                        final EditText angerInput = new EditText(DailyActivity.this);
+                        AlertDialog.Builder angerBuilder = new AlertDialog.Builder(DailyActivity.this);
+                        angerBuilder.setTitle("怼一怼更健康").setIcon(android.R.drawable.ic_input_get)
+                                .setView(angerInput).setNegativeButton("算了猪猪辣么可爱", null);
+                        angerBuilder.setPositiveButton("滚啊", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                new Thread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Socket socket;
+                                        try {
+                                            socket = new Socket("192.168.1.184", 4497);
+
+                                            String socketData = "daily8:" + angerInput.getText().toString();
+                                            BufferedWriter writer = new BufferedWriter(
+                                                    new OutputStreamWriter(socket.getOutputStream()));
+                                            writer.write(socketData + "\0");
+                                            writer.flush();
+                                            socket.close();
+                                            Looper.prepare();
+                                            Toast.makeText(getApplicationContext(), "我，我错了…别怼漏气了就好…",
+                                                    Toast.LENGTH_SHORT).show();
+                                            Looper.loop();
+
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                            Looper.prepare();
+                                            Toast.makeText(getApplicationContext(), "哼唧哼唧，出问题了…",
+                                                    Toast.LENGTH_SHORT).show();
+                                            Looper.loop();
+                                        }
+                                    }
+                                }).start();
+                            }
+                        });
+                        angerBuilder.show();
                         break;
                     case 9:
+                        final EditText dreamInput = new EditText(DailyActivity.this);
+                        AlertDialog.Builder dreamBuilder = new AlertDialog.Builder(DailyActivity.this);
+                        dreamBuilder.setTitle("兔子又在期待什么呀").setIcon(android.R.drawable.ic_input_get)
+                                .setView(dreamInput).setNegativeButton("算了没什么", null);
+                        dreamBuilder.setPositiveButton("就是这个", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                new Thread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Socket socket;
+                                        try {
+                                            socket = new Socket("192.168.1.184", 4497);
+
+                                            String socketData = "daily9:" + dreamInput.getText().toString();
+                                            BufferedWriter writer = new BufferedWriter(
+                                                    new OutputStreamWriter(socket.getOutputStream()));
+                                            writer.write(socketData + "\0");
+                                            writer.flush();
+                                            socket.close();
+                                            Looper.prepare();
+                                            Toast.makeText(getApplicationContext(), "猜猜什么时候愿望成真呢？",
+                                                    Toast.LENGTH_SHORT).show();
+                                            Looper.loop();
+
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                            Looper.prepare();
+                                            Toast.makeText(getApplicationContext(), "哼唧哼唧，出问题了…",
+                                                    Toast.LENGTH_SHORT).show();
+                                            Looper.loop();
+                                        }
+                                    }
+                                }).start();
+                            }
+                        });
+                        dreamBuilder.show();
                         break;
                     case 10:
+                        Toast.makeText(getApplicationContext(), "小兔崽子没出生呢，别着急~",
+                                Toast.LENGTH_SHORT).show();
                         break;
                     case 11:
-                        break;
-                    case 12:
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Socket socket;
+                                try {
+                                    socket = new Socket("192.168.1.184", 4497);
+
+                                    String socketData = "daily11:";
+                                    BufferedWriter writer = new BufferedWriter(
+                                            new OutputStreamWriter(socket.getOutputStream()));
+                                    writer.write(socketData + "\0");
+                                    writer.flush();
+                                    socket.close();
+                                    Looper.prepare();
+                                    Toast.makeText(getApplicationContext(), "兔兔晚安~",
+                                            Toast.LENGTH_SHORT).show();
+                                    Looper.loop();
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                    Looper.prepare();
+                                    Toast.makeText(getApplicationContext(), "哼唧哼唧，出问题了…",
+                                            Toast.LENGTH_SHORT).show();
+                                    Looper.loop();
+                                }
+                            }
+                        }).start();
                         break;
                 }
             }
@@ -262,7 +367,7 @@ public class DailyActivity extends AppCompatActivity{
                 }
             }
             else if (requestCode == SUB_CAMERA){
-                if (data.getData() != null|| data.getExtras() != null){
+                if (data != null && (data.getData() != null|| data.getExtras() != null)){
                     Uri uri =data.getData();
                     Bitmap photo = null;
                     if (uri != null) {
--- BunnyPiggy/app/src/main/java/supergp/bunnypiggy/LoverActivity.java
+++ BunnyPiggy/app/src/main/java/supergp/bunnypiggy/LoverActivity.java
@@ -1,7 +1,13 @@
 package supergp.bunnypiggy;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class LoverActivity extends AppCompatActivity {
 
@@ -9,5 +15,41 @@ public class LoverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lover);
+        onRefresh();
+
+        final TextView dateText;
+        final int year,month,day;
+        Calendar cal;
+
+        cal=Calendar.getInstance();
+        year=cal.get(Calendar.YEAR);
+        month=cal.get(Calendar.MONTH);
+        day=cal.get(Calendar.DAY_OF_MONTH);
+
+        dateText=(TextView) findViewById(R.id.date);
+        if (dateText != null) {
+            dateText.setText(String.valueOf(year) + '-' +
+                    String.valueOf(month + 1) + '-' + String.valueOf(day));
+            dateText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    DatePickerDialog.OnDateSetListener listener=new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker arg0, int year, int month, int day) {
+                            dateText.setText(year+"-"+(++month)+"-"+day);
+                            onRefresh();
+                        }
+                    };
+                    DatePickerDialog dialog=new DatePickerDialog(LoverActivity.this, 0,listener,year,month,day);
+                    dialog.show();
+                }
+            });
+        }
+    }
+
+    void onRefresh(){
+
     }
 }
