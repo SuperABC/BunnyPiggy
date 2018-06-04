0839f71f90ea6804135796d161f7edacb2ffd4a3
SG master <1363028533@qq.com>
2018-06-05 00:08:02 +0800

    Implement a little in joke.

--- BunnyPiggy/app/src/main/java/supergp/bunnypiggy/JokeActivity.java
+++ BunnyPiggy/app/src/main/java/supergp/bunnypiggy/JokeActivity.java
@@ -2,6 +2,7 @@ package supergp.bunnypiggy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class JokeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+
     }
 }
